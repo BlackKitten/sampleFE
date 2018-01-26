5c26b9cbc3de3ddf8df1568d05ae887dd444b429
developer <dev_groeipakket@kindengezin.be>
2018-01-26 10:51:14 +0100

    benbe: removed incorrect env var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-var port = (process.env.PORT || SAMPLEFE_SERVICE_PORT || process.env.OPENSHIFT_NODEJS_PORT || 4200);
+var port = (process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.use(express.static('dist'))
 
 app.listen(port, () => console.log('Example app listening on port', port))
